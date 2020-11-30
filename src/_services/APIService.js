5d7cdcc119779ffb71152cee05b4cacf7eb5af13
wmtowne <wmtowne@svsu.edu>
2020-11-29 20:31:35 -0800

    added serve so app could run own web server

--- src/_services/APIService.js
+++ src/_services/APIService.js
@@ -1,1 +1,10 @@
 export const baseURI = "https://wels-fit4lifebackend.herokuapp.com";
+
+/*
+ "scripts": {
+    "start": "react-scripts start",
+    "build": "react-scripts build",
+    "test": "react-scripts test",
+    "eject": "react-scripts eject"
+  },
+  */
